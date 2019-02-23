5b2fa85523f46dfef2d365af002cf769fc7a9683
Igor Bruno <ig40bruno@gmail.com>
2019-02-23 18:01:37 -0300

    Realizei a configuração do projeto

---
+++ src/main/java/br/com/wscontazul/WscontazulApplication.java
@@ -0,0 +1,13 @@
+package br.com.wscontazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WscontazulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WscontazulApplication.class, args);
+	}
+
+}
